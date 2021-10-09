6924af8c1d156f65bfed6a1e6a2afbf90ec95c88
Ivan-Slavkovic <ivanslavkovic.is@gmail.com>
2021-10-09 21:42:26 +0200

    useMemo practice

--- src/ProductsList.jsx
+++ src/ProductsList.jsx
@@ -1,13 +1,16 @@
 import react from "react";
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { CategoriesService, BrandsService, SortService } from "./Service";
+
 function ProductsList(props) {
   //state
-  let [search, setSearch] = useState("");
-  let [products, setProducts] = useState([]);
-  let [sortBy, setSortBy] = useState("productName");
+  let [search, setSearch] = useState(""); //represents the value of the search textbox
+  let [products, setProducts] = useState([]); //represents the array of products to show in the grid
+  let [sortBy, setSortBy] = useState("productName"); //represents name of the sort column selected by user
   let [sortOrder, setSortOrder] = useState("ASC"); //ASC or DESC aka Ascending and Descending
-  let [originalProducts, setOriginalProducts] = useState([]);
+  let [originalProducts, setOriginalProducts] = useState([]); //stores the actual array of products loaded from server
+  let [brands, setBrands] = useState([]); //stores all brands from server
+  let [selectedBrand, setSelectedBrand] = useState(""); //represents name of the brand selected by the user
 
   //useEffect - execute a callback function when the compoent is rendered for the first time
   useEffect(() => {
@@ -15,6 +18,7 @@ function ProductsList(props) {
       //request to brands table
       let brandsResponse = await BrandsService.fetchBrands();
       let brandsResponseBody = await brandsResponse.json();
+      setBrands(brandsResponseBody);
 
       //request to categories table
       let categoriesResponse = await CategoriesService.fetchCategories();
@@ -44,16 +48,27 @@ function ProductsList(props) {
     })();
   }, [search]);
 
+  let filteredProducts = useMemo(() => {
+    console.log("filtered products", originalProducts, selectedBrand);
+    return originalProducts.filter(
+      (prod) => prod.brand.brandName.indexOf(selectedBrand) >= 0
+    );
+  }, [originalProducts, selectedBrand]);
+
   //when the user click on a column name to sort
   let onSortColumnNameClick = (event, columnName) => {
     event.preventDefault(); //avoid refresh
     setSortBy(columnName);
     let negatedSortOrder = sortOrder === "ASC" ? "DESC" : "ASC";
     setSortOrder(negatedSortOrder);
-    setProducts(
-      SortService.getSortedArray(originalProducts, columnName, negatedSortOrder)
-    );
   };
+
+  //useEffect: Executes on each change of filteredBrands, sortBy or sortOrder
+  useEffect(() => {
+    setProducts(
+      SortService.getSortedArray(filteredProducts, sortBy, sortOrder)
+    );
+  }, [filteredProducts, sortBy, sortOrder]);
 
   let getColumnHeader = (columnName, displayName) => {
     return (
@@ -92,7 +107,7 @@ function ProductsList(props) {
           </div>
         </div>
 
-        <div className="col-lg-9">
+        <div className="col-lg-6">
           <input
             type="search"
             placeholder="Search"
@@ -103,6 +118,24 @@ function ProductsList(props) {
               setSearch(event.target.value);
             }}
           ></input>
+        </div>
+        <div className="col-lg-3">
+          <select
+            className="form-control"
+            value={selectedBrand}
+            onChange={(event) => {
+              setSelectedBrand(event.target.value);
+            }}
+          >
+            <option value="">All Brands</option>
+            {brands.map((brand) => {
+              return (
+                <option value={brand.brandName} key={brand.id}>
+                  {brand.brandName}
+                </option>
+              );
+            })}
+          </select>
         </div>
       </div>
 
