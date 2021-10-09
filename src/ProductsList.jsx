7411976a134b21b895a1677bb2f5bd2bb3ac3cd1
Ivan-Slavkovic <ivanslavkovic.is@gmail.com>
2021-10-09 20:46:31 +0200

    Grid - Sort option

--- src/ProductsList.jsx
+++ src/ProductsList.jsx
@@ -1,9 +1,13 @@
+import react from "react";
 import React, { useState, useEffect, useCallback } from "react";
-import { ProductsService, CategoriesService, BrandsService } from "./Service";
+import { CategoriesService, BrandsService, SortService } from "./Service";
 function ProductsList(props) {
   //state
   let [search, setSearch] = useState("");
   let [products, setProducts] = useState([]);
+  let [sortBy, setSortBy] = useState("productName");
+  let [sortOrder, setSortOrder] = useState("ASC"); //ASC or DESC aka Ascending and Descending
+  let [originalProducts, setOriginalProducts] = useState([]);
 
   //useEffect - execute a callback function when the compoent is rendered for the first time
   useEffect(() => {
@@ -17,7 +21,10 @@ function ProductsList(props) {
       let categoriesResponseBody = await categoriesResponse.json();
 
       //request to products table
-      let productsResponse = await ProductsService.fetchProducts();
+      let productsResponse = await fetch(
+        `http://localhost:5000/products?productName_like=${search}&_sort=productName&_order=ASC`,
+        { method: "GET" }
+      );
       let productsResponseBody = await productsResponse.json();
 
       //setCategory property into each product
@@ -33,9 +40,45 @@ function ProductsList(props) {
       });
 
       setProducts(productsResponseBody);
+      setOriginalProducts(productsResponseBody);
     })();
-  }, []);
+  }, [search]);
 
+  //when the user click on a column name to sort
+  let onSortColumnNameClick = (event, columnName) => {
+    event.preventDefault(); //avoid refresh
+    setSortBy(columnName);
+    let negatedSortOrder = sortOrder === "ASC" ? "DESC" : "ASC";
+    setSortOrder(negatedSortOrder);
+    setProducts(
+      SortService.getSortedArray(originalProducts, columnName, negatedSortOrder)
+    );
+  };
+
+  let getColumnHeader = (columnName, displayName) => {
+    return (
+      <React.Fragment>
+        <a
+          href="/#"
+          onClick={(event) => {
+            onSortColumnNameClick(event, columnName);
+          }}
+        >
+          {displayName}
+        </a>{" "}
+        {sortBy === columnName && sortOrder === "ASC" ? (
+          <i className="fa fa-sort-up"></i>
+        ) : (
+          ""
+        )}
+        {sortBy === columnName && sortOrder === "DESC" ? (
+          <i className="fa fa-sort-down"></i>
+        ) : (
+          ""
+        )}
+      </React.Fragment>
+    );
+  };
   return (
     <div className="row">
       <div className="col-12">
@@ -43,7 +86,7 @@ function ProductsList(props) {
           <div className="col-lg-3">
             <h4>
               <i className="fa fa-suitcase"></i>
-              &nbsp; Products &nbsp
+              &nbsp; Products &nbsp;
               <span className="badge badge-secondary">{products.length}</span>
             </h4>
           </div>
@@ -69,19 +112,17 @@ function ProductsList(props) {
             <table className="table">
               <thead>
                 <tr>
-                  <th>#</th>
-                  <th>Product Name</th>
-                  <th>Price</th>
-                  <th>Brand</th>
-                  <th>Category</th>
-                  <th>Rating</th>
+                  <th>{getColumnHeader("productName", "Product Name")}</th>
+                  <th>{getColumnHeader("price", "Price")}</th>
+                  <th>{getColumnHeader("brand", "Brand")}</th>
+                  <th>{getColumnHeader("category", "Category")}</th>
+                  <th>{getColumnHeader("rating", "Rating")}</th>
                 </tr>
               </thead>
               <tbody>
                 {products.map((product) => {
                   return (
                     <tr key={product.id}>
-                      <td>{product.id}</td>
                       <td>{product.productName}</td>
                       <td>{product.price}</td>
                       <td>{product.brand.brandName}</td>
